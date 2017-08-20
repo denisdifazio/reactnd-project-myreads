7fecdc83c6d104a3f69e9aab86f9fd503031753b
denisdifazio <denisdifazio@gmail.com>
2017-08-20 18:45:41 -0300

    refactor: Adding suggestion from code review

--- src/classes/BooksList.js
+++ src/classes/BooksList.js
@@ -7,20 +7,39 @@ class BooksList extends Component {
     static propTypes = {
         books: PropTypes.array.isRequired,
         onUpdateBookShelf: PropTypes.func.isRequired
-    }
-
+    };     
+   
     render() {
-        const books = this.props.books;
+        const books = this.props.books; 
+        
+        const shelves = [
+            {
+                id: 'currentlyReading',
+                title: 'Currently Reading',
+                books: books.filter(book => book.shelf === 'currentlyReading')
+            },
+            {
+                id: 'wantToRead',
+                title: 'Want to Read',
+                books: books.filter(book => book.shelf === 'wantToRead')
+            },
+            {
+                id: 'read',
+                title: 'Read',
+                books: books.filter(book => book.shelf === 'read')
+            }
+        ];
+
         return (      
             <div className="list-books">
                 <div className="list-books-title">
                     <h1>MyReads</h1>
                 </div>
                 <div className="list-books-content">
-                    <div>
-                        <Bookshelf name='Currently Reading' books={books.filter((book) => (book.shelf === 'currentlyReading'))} onUpdateBookShelf={this.props.onUpdateBookShelf}/>
-                        <Bookshelf name='Want to Read' books={books.filter((book) => (book.shelf === 'wantToRead'))} onUpdateBookShelf={this.props.onUpdateBookShelf}/>
-                        <Bookshelf name='Read' books={books.filter((book) => (book.shelf === 'read'))} onUpdateBookShelf={this.props.onUpdateBookShelf}/>   
+                    <div>    
+                        {shelves.map(shelf => (
+                            <Bookshelf key={shelf.id} name={shelf.title} books={shelf.books} onUpdateBookShelf={this.props.onUpdateBookShelf}/>
+                        ))}                   
                     </div>                                                          
                     <div className="open-search">
                         <Link to="/search">Add a book</Link>
--- src/classes/BooksSearch.js
+++ src/classes/BooksSearch.js
@@ -27,6 +27,14 @@ class BooksSearch extends Component {
         }
     }
 
+    getBookShelf = (book) => {
+        let auxBook = this.props.booksOnShelves.find( bookShelf => bookShelf.id === book.id);
+        if (auxBook) {
+            return auxBook; 
+        }
+        return book;
+    }
+
     onUpdateQuery = (query) => {
         this.setState({ query });
         clearTimeout(this.typingTimer);
@@ -37,15 +45,7 @@ class BooksSearch extends Component {
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to="/" className="close-search">Close</Link>
-                <div className="search-books-input-wrapper">
-                    {/* 
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                    
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                    */}
+                <div className="search-books-input-wrapper">                    
                     <input
                         autoFocus
                         type="text"
@@ -57,10 +57,8 @@ class BooksSearch extends Component {
                 <div className="search-books-results">
                     <ol className="books-grid">
                         {this.state.showingBooks.map(book => {
-                            let auxBook = this.props.booksOnShelves.find( bookShelf => bookShelf.id === book.id);
-                            if (auxBook) {
-                                book = auxBook;
-                            }
+                            book = this.getBookShelf(book);
+                            
                             return (<li key={book.id}>  
                                 <Book book={book} onUpdateBookShelf={this.props.onUpdateBookShelf}/>                              
                             </li>)
